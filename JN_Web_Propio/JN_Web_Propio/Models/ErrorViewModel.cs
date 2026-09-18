2d375e6411a295f6bf37bf09cfe0391bd0bea689
paomalavassi <paomalavassi00@gmail.com>
2026-09-17 19:35:45 -0600

    Commit inicial

---
+++ JN_Web_Propio/JN_Web_Propio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JN_Web_Propio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
