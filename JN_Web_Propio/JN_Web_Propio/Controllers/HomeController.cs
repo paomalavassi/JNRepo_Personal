ac3676bab8e25ad668b3e74ef0fc67d28526ca3f
paomalavassi <paomalavassi00@gmail.com>
2026-09-17 21:12:24 -0600

    Semana 2

--- JN_Web_Propio/JN_Web_Propio/Controllers/HomeController.cs
+++ JN_Web_Propio/JN_Web_Propio/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using JN_Web_Propio.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -11,15 +11,5 @@ namespace JN_Web_Propio.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- JN_Web_Propio/JN_Web_Propio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JN_Web_Propio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
